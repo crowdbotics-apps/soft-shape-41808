d3291c216f2e69a92feaf298bc2f579570a4b6c5
Crowdbotics <team@crowdbotics.com>
2023-06-29 13:20:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-shape-41808",
+  url: "https://soft-shape-41808.botics.co",
+  api: "https://soft-shape-41808.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
